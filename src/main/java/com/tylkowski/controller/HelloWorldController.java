c22deb10719da80ea7ef9c7b5276f60c725b9eb8
Hubert Tylkowski <hubert.tylkowski@gmail.com>
2017-07-15 00:26:27 +0200

    Uploaded

---
+++ src/main/java/com/tylkowski/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.tylkowski.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String showHelloWorld() {
+        return "welcome-world";
+    }
+}
